3738288eec6fe5a7d7edc46667bfc3f1026d101e
a.mean.blogger <a.mean.blogger@gmail.com>
2016-11-01 08:16:21 -0700

    user schema validation/error handling added

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ var express        = require("express");
 var mongoose       = require("mongoose");
 var bodyParser     = require("body-parser");
 var methodOverride = require("method-override");
+var flash          = require("connect-flash");
+var session        = require("express-session");
 var app = express();
 
 // DB setting
@@ -20,6 +22,8 @@ app.use(express.static(__dirname+"/public"));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended:true}));
 app.use(methodOverride("_method"));
+app.use(flash());
+app.use(session({secret:"MySecret"}));
 
 // Routes
 app.use("/", require("./routes/home"));
