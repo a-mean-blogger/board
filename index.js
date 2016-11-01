6e3f96df43086dafe8b22a639ba954fef4009d34
a.mean.blogger <a.mean.blogger@gmail.com>
2016-11-01 08:14:25 -0700

    user model, route, views added

--- index.js
+++ index.js
@@ -24,6 +24,7 @@ app.use(methodOverride("_method"));
 // Routes
 app.use("/", require("./routes/home"));
 app.use("/posts", require("./routes/posts"));
+app.use("/users", require("./routes/users"));
 
 // Port setting
 app.listen(3000, function(){
---
+++ routes/users.js
@@ -0,0 +1,66 @@
+var express  = require("express");
+var router   = express.Router();
+var User     = require("../models/User");
+
+// Index
+router.route("/").get(function(req, res){
+  User.find({})
+  .sort({username:1})
+  .exec(function(err, users){
+    if(err) return res.json(err);
+    res.render("users/index", {users:users});
+  });
+});
+
+// New
+router.get("/new", function(req, res){
+  res.render("users/new", {user:{}});
+});
+
+// create
+router.post("/", function(req, res){
+  User.create(req.body, function(err, user){
+    if(err) return res.json(err);
+    res.redirect("/users");
+  });
+});
+
+// show
+router.get("/:username", function(req, res){
+  User.findOne({username:req.params.username}, function(err, user){
+    if(err) return res.json(err);
+    res.render("users/show", {user:user});
+  });
+});
+
+// edit
+router.get("/:username/edit", function(req, res){
+  User.findOne({username:req.params.username}, function(err, user){
+    if(err) return res.json(err);
+    res.render("users/edit", {user:user});
+  });
+});
+
+// update
+router.put("/:username",function(req, res, next){
+  User.findOne({username:req.params.username})
+  .select({password:1})
+  .exec(function(err, user){
+    if(err) return res.json(err);
+
+    // update user object
+    user.originalPassword = user.password;
+    user.password = req.body.newPassword? req.body.newPassword : user.password;
+    for(var p in req.body){
+      user[p] = req.body[p];
+    }
+
+    // save updated user
+    user.save(function(err, user){
+      if(err) return res.json(err);
+      res.redirect("/users/"+req.params.username);
+    });
+  });
+});
+
+module.exports = router;
