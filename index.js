5ee26ee10f14f4f1f566fdefbf0dac7ef088cd02
a.mean.blogger <a.mean.blogger@gmail.com>
2020-01-03 15:56:12 -0800

    navbar, welcome/about page added

---
+++ index.js
@@ -0,0 +1,31 @@
+var express = require('express');
+var mongoose = require('mongoose');
+var bodyParser = require('body-parser');
+var methodOverride = require('method-override');
+var app = express();
+
+// DB setting
+mongoose.connect(process.env.MONGO_DB);
+var db = mongoose.connection;
+db.once('open', function(){
+  console.log('DB connected');
+});
+db.on('error', function(err){
+  console.log('DB ERROR : ', err);
+});
+
+// Other settings
+app.set('view engine', 'ejs');
+app.use(express.static(__dirname+'/public'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended:true}));
+app.use(methodOverride('_method'));
+
+// Routes
+app.use('/', require('./routes/home'));
+
+// Port setting
+var port = 3000;
+app.listen(port, function(){
+  console.log('server on! http://localhost:'+port);
+});
