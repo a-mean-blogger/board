a0acab9294e4ee224451c4580b29e685282113e6
a.mean.blogger <a.mean.blogger@gmail.com>
2020-01-03 16:01:44 -0800

    post schema validation/error handling added, util.js added

--- models/Post.js
+++ models/Post.js
@@ -2,8 +2,8 @@ var mongoose = require('mongoose');
 
 // schema
 var postSchema = mongoose.Schema({
-  title:{type:String, required:true},
-  body:{type:String, required:true},
+  title:{type:String, required:[true,'Title is required!']},
+  body:{type:String, required:[true,'Body is required!']},
   createdAt:{type:Date, default:Date.now},
   updatedAt:{type:Date},
 });
