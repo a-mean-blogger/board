ce14a6034864819d02fbea0d7eb06d775778be3d
a.mean.blogger <a.mean.blogger@gmail.com>
2020-01-03 16:02:43 -0800

    relationship between posts and users added

--- models/Post.js
+++ models/Post.js
@@ -4,6 +4,7 @@ var mongoose = require('mongoose');
 var postSchema = mongoose.Schema({
   title:{type:String, required:[true,'Title is required!']},
   body:{type:String, required:[true,'Body is required!']},
+  author:{type:mongoose.Schema.Types.ObjectId, ref:'user', required:true},
   createdAt:{type:Date, default:Date.now},
   updatedAt:{type:Date},
 });
