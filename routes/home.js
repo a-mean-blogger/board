2ff5110b73b24aed41560404be17ce141d9e2655
a.mean.blogger <a.mean.blogger@gmail.com>
2020-01-03 16:00:21 -0800

    login added

--- routes/home.js
+++ routes/home.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var router = express.Router();
+var passport = require('../config/passport');
 
 // Home
 router.get('/', function(req, res){
@@ -7,6 +8,51 @@ router.get('/', function(req, res){
 });
 router.get('/about', function(req, res){
   res.render('home/about');
+});
+
+// Login
+router.get('/login', function (req,res) {
+  var username = req.flash('username')[0];
+  var errors = req.flash('errors')[0] || {};
+  res.render('home/login', {
+    username:username,
+    errors:errors
+  });
+});
+
+// Post Login
+router.post('/login',
+  function(req,res,next){
+    var errors = {};
+    var isValid = true;
+
+    if(!req.body.username){
+      isValid = false;
+      errors.username = 'Username is required!';
+    }
+    if(!req.body.password){
+      isValid = false;
+      errors.password = 'Password is required!';
+    }
+
+    if(isValid){
+      next();
+    }
+    else {
+      req.flash('errors',errors);
+      res.redirect('/login');
+    }
+  },
+  passport.authenticate('local-login', {
+    successRedirect : '/posts',
+    failureRedirect : '/login'
+  }
+));
+
+// Logout
+router.get('/logout', function(req, res) {
+  req.logout();
+  res.redirect('/');
 });
 
 module.exports = router;
