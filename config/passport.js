4eac8068633401fcc8be0b15f915a63d86287d10
a.mean.blogger <a.mean.blogger@gmail.com>
2020-01-03 16:00:21 -0800

    login added

---
+++ config/passport.js
@@ -0,0 +1,41 @@
+var passport = require('passport');
+var LocalStrategy = require('passport-local').Strategy;
+var User = require('../models/User');
+
+// serialize & deserialize User
+passport.serializeUser(function(user, done) {
+  done(null, user.id);
+});
+passport.deserializeUser(function(id, done) {
+  User.findOne({_id:id}, function(err, user) {
+    done(err, user);
+  });
+});
+
+// local strategy
+passport.use('local-login',
+  new LocalStrategy({
+      usernameField : 'username',
+      passwordField : 'password',
+      passReqToCallback : true
+    },
+    function(req, username, password, done) {
+      User.findOne({username:username})
+        .select({password:1})
+        .exec(function(err, user) {
+          if (err) return done(err);
+
+          if (user && user.authenticate(password)){
+            return done(null, user);
+          }
+          else {
+            req.flash('username', username);
+            req.flash('errors', {login:'The username or password is incorrect.'});
+            return done(null, false);
+          }
+        });
+    }
+  )
+);
+
+module.exports = passport;
--- index.js
+++ index.js
@@ -4,6 +4,7 @@ var bodyParser = require('body-parser');
 var methodOverride = require('method-override');
 var flash = require('connect-flash');
 var session = require('express-session');
+var passport = require('./config/passport');
 var app = express();
 
 // DB setting
@@ -28,6 +29,17 @@ app.use(bodyParser.urlencoded({extended:true}));
 app.use(methodOverride('_method'));
 app.use(flash());
 app.use(session({secret:'MySecret', resave:true, saveUninitialized:true}));
+
+// Passport
+app.use(passport.initialize());
+app.use(passport.session());
+
+// Custom Middlewares
+app.use(function(req,res,next){
+  res.locals.isAuthenticated = req.isAuthenticated();
+  res.locals.currentUser = req.user;
+  next();
+});
 
 // Routes
 app.use('/', require('./routes/home'));
