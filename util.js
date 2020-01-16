c546f73e0ab79a17721c0bef8eeac355e9fae8d9
a.mean.blogger <a.mean.blogger@gmail.com>
2020-01-16 15:44:26 -0800

    route restriction added

--- util.js
+++ util.js
@@ -17,4 +17,20 @@ util.parseError = function(errors){
   return parsed;
 }
 
+util.isLoggedin = function(req, res, next){
+  if(req.isAuthenticated()){
+    next();
+  }
+  else {
+    req.flash('errors', {login:'Please login first'});
+    res.redirect('/login');
+  }
+}
+
+util.noPermission = function(req, res){
+  req.flash('errors', {login:"You don't have permission"});
+  req.logout();
+  res.redirect('/login');
+}
+
 module.exports = util;
