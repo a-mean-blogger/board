88487987ae6e6b8297986896920fcd6f5fe63c53
a.mean.blogger <a.mean.blogger@gmail.com>
2020-01-16 15:46:47 -0800

    search 1 - title/body search

--- util.js
+++ util.js
@@ -39,9 +39,13 @@ util.getPostQueryString = function(req, res, next){
     var queryArray = [];
     var page = overwrites.page?overwrites.page:(req.query.page?req.query.page:'');
     var limit = overwrites.limit?overwrites.limit:(req.query.limit?req.query.limit:'');
+    var searchType = overwrites.searchType?overwrites.searchType:(req.query.searchType?req.query.searchType:'');
+    var searchText = overwrites.searchText?overwrites.searchText:(req.query.searchText?req.query.searchText:'');
 
     if(page) queryArray.push('page='+page);
     if(limit) queryArray.push('limit='+limit);
+    if(searchType) queryArray.push('searchType='+searchType);
+    if(searchText) queryArray.push('searchText='+searchText);
 
     if(queryArray.length>0) queryString = (isAppended?'&':'?') + queryArray.join('&');
 
