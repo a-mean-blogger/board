64a24a2f0628ee033f8f667dbbc62dd9779a4f7e
a.mean.blogger <a.mean.blogger@gmail.com>
2020-01-16 15:48:49 -0800

    comment 2 - update/destroy functions added

--- routes/comments.js
+++ routes/comments.js
@@ -20,9 +20,49 @@ router.post('/', util.isLoggedin, checkPostId, function(req, res){
   });
 });
 
+// update
+router.put('/:id', util.isLoggedin, checkPermission, checkPostId, function(req, res){
+  var post = res.locals.post;
+
+  req.body.updatedAt = Date.now();
+  Comment.findOneAndUpdate({_id:req.params.id}, req.body, {runValidators:true}, function(err, comment){
+    if(err){
+      req.flash('commentForm', { _id:req.params.id, form:req.body });
+      req.flash('commentError', { _id:req.params.id, errors:util.parseError(err) });
+    }
+    return res.redirect('/posts/'+post._id+res.locals.getPostQueryString());
+  });
+});
+
+// destroy
+router.delete('/:id', util.isLoggedin, checkPermission, checkPostId, function(req, res){
+  var post = res.locals.post;
+
+  Comment.findOne({_id:req.params.id}, function(err, comment){
+    if(err) return res.json(err);
+
+    // save updated comment
+    comment.isDeleted = true;
+    comment.save(function(err, comment){
+      if(err) return res.json(err);
+
+      return res.redirect('/posts/'+post._id+res.locals.getPostQueryString());
+    });
+  });
+});
+
 module.exports = router;
 
 // private functions
+function checkPermission(req, res, next){
+  Comment.findOne({_id:req.params.id}, function(err, comment){
+    if(err) return res.json(err);
+    if(comment.author != req.user.id) return util.noPermission(req, res);
+
+    next();
+  });
+}
+
 function checkPostId(req, res, next){
   Post.findOne({_id:req.query.postId}, function(err, post){
     if(err) return res.json(err);
