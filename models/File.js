adf8b5c40b6087bd095b78a2fb48583a466f954c
a.mean.blogger <a.mean.blogger@gmail.com>
2020-02-19 14:38:57 -0800

    file attachment 1 - uploading

---
+++ models/File.js
@@ -0,0 +1,27 @@
+var mongoose = require('mongoose');
+
+// schema
+var fileSchema = mongoose.Schema({
+  originalFileName:{type:String},
+  serverFileName:{type:String},
+  size:{type:Number},
+  uploadedBy:{type:mongoose.Schema.Types.ObjectId, ref:'user', required:true},
+  postId:{type:mongoose.Schema.Types.ObjectId, ref:'post'},
+  isDeleted:{type:Boolean, default:false},
+});
+
+// model & export
+var File = mongoose.model('file', fileSchema);
+
+// model methods
+File.createNewInstance = async function(file, uploadedBy, postId){
+  return await File.create({
+      originalFileName:file.originalname,
+      serverFileName:file.filename,
+      size:file.size,
+      uploadedBy:uploadedBy,
+      postId:postId,
+    });
+};
+
+module.exports = File;
