64ac875b11265b0d7dbc200de3f1bac5173aeb50
a.mean.blogger <a.mean.blogger@gmail.com>
2020-08-06 09:38:20 -0700

    file attachment 2 - downloading

--- models/File.js
+++ models/File.js
@@ -1,4 +1,6 @@
 var mongoose = require('mongoose');
+var fs = require('fs');
+var path = require('path');
 
 // schema
 var fileSchema = mongoose.Schema({
@@ -9,6 +11,24 @@ var fileSchema = mongoose.Schema({
   postId:{type:mongoose.Schema.Types.ObjectId, ref:'post'},
   isDeleted:{type:Boolean, default:false},
 });
+
+// instance methods
+fileSchema.methods.processDelete = function(){
+  this.isDeleted = true;
+  this.save();
+};
+fileSchema.methods.getFileStream = function(){
+  var stream;
+  var filePath = path.join(__dirname,'..','uploadedFiles',this.serverFileName);
+  var fileExists = fs.existsSync(filePath);
+  if(fileExists){
+    stream = fs.createReadStream(filePath);
+  }
+  else {
+    this.processDelete();
+  }
+  return stream;
+};
 
 // model & export
 var File = mongoose.model('file', fileSchema);
